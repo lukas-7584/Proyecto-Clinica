7742df2959854514560e79c3b55f6eb7ff89b8d0
lucas <amaya.jorgelucas@gmail.com>
2022-03-14 16:19:01 -0300

    modificaciones ne  la navbar

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ( ){
+export default function NavBar ( ){
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light ">
                 <div class="container-fluid">
